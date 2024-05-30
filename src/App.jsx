6b65769cc7c74520ca8746b54062dc4f3c300ae0
nnebuoukamaka <ukysmart96@gmail.com>
2024-05-30 15:39:19 +0100

    implemented the footer of the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <div>
         <NavBar />
+        <Footer/>
       </div>
       
     </>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,80 +13,77 @@ import likeIcon from "../assets/navbarImages/likeIcon.svg";
 import "../styles/NavBar.css";
 function NavBar() {
   return (
-    <div>
-      <header className="header">
-        <nav>
-          <div className="upper-navbar">
-            <ul className="uppernavbar-list1">
-              <li style={{ display: "flex" }}>
-                <img src={callIcon} alt="call" />
-                <span style={{ marginLeft: "5px" }}>(225) 555-0118 </span>
-              </li>
-              <li style={{ display: "flex" }}>
-                <img src={messageIcon} alt="email" />
-                <span style={{ marginLeft: "5px" }}>
-                  michelle.rivera@gmail.com
-                </span>
-              </li>
-            </ul>
-            <div>
-              <h6>Follow Us and get a chance to win 80% off</h6>
-            </div>
-            <ul className="uppernavbar-list2">
-              <li>
-                <h6> Follow Us :</h6>
-              </li>
-              <li>
-                <img src={instagramIcon} alt="Instagram" />
-              </li>
-              <li>
-                <img src={youtubeIconIcon} alt="Youtube" />
-              </li>
-              <li>
-                <img src={facebookIcon} alt="Facebook" />
-              </li>
-              <li>
-                <img src={xIcon} alt="X" />
-              </li>
-            </ul>
+    <header className="header">
+      <nav>
+        <div className="upper-navbar">
+          <ul className="uppernavbar-list1">
+            <li style={{ display: "flex" }}>
+              <img src={callIcon} alt="call" />
+              <span style={{ marginLeft: "5px" }}>(225) 555-0118 </span>
+            </li>
+            <li style={{ display: "flex" }}>
+              <img src={messageIcon} alt="email" />
+              <span style={{ marginLeft: "5px" }}>
+                michelle.rivera@gmail.com
+              </span>
+            </li>
+          </ul>
+          <div>
+            <h6>Follow Us and get a chance to win 80% off</h6>
           </div>
-          <div className="lower-navbar">
-            <div>
-              <h3>Bandage</h3>
-            </div>
-            <ul className="lower-navbar-list1">
-              <li>Home</li>
-              <li>
-                Shop
-                <img src={dropdownIcon} style={{marginLeft:'8px'}}/>
-              </li>
-              <li>About</li>
-              <li>Blog</li>
-              <li>Contact</li>
-              <li>Pages</li>
-            </ul>
-            <ul className="lower-navbar-list2">
-              <li>
-                <img src={loginIcon} style={{marginRight:'5px'}} />
-                Login / Register
-              </li>
-              <li>
-                <img src={searchIcon} />
-              </li>
-              <li style={{display:'flex', columnGap:'3px'}}>
-                <img src={cartIcon} />
-                <span>1</span>
-              </li>
-              <li style={{display:'flex', columnGap:'3px'}}>
-                <img src={likeIcon} />
-                <span>1</span>
-              </li>
-            </ul>
-            <div></div>
-          </div>
-        </nav>
-      </header>
-    </div>
+          <ul className="uppernavbar-list2">
+            <li>
+              <h6> Follow Us :</h6>
+            </li>
+            <li>
+              <img src={instagramIcon} alt="Instagram" />
+            </li>
+            <li>
+              <img src={youtubeIconIcon} alt="Youtube" />
+            </li>
+            <li>
+              <img src={facebookIcon} alt="Facebook" />
+            </li>
+            <li>
+              <img src={xIcon} alt="X" />
+            </li>
+          </ul>
+        </div>
+        <div className="lower-navbar">
+          <h3>Bandage</h3>
+
+          <ul className="lower-navbar-list1">
+            <li>Home</li>
+            <li>
+              Shop
+              <img src={dropdownIcon} style={{ marginLeft: "8px" }} />
+            </li>
+            <li>About</li>
+            <li>Blog</li>
+            <li>Contact</li>
+            <li>Pages</li>
+          </ul>
+          <ul className="lower-navbar-list2">
+            <li>
+              <img src={loginIcon} style={{ marginRight: "5px" }} />
+              Login / Register
+            </li>
+            <li>
+              <img src={searchIcon} />
+            </li>
+            <li style={{ display: "flex", columnGap: "3px" }}>
+              <img src={cartIcon} />
+              <span>1</span>
+            </li>
+            <li style={{ display: "flex", columnGap: "3px" }}>
+              <img src={likeIcon} />
+              <span>1</span>
+            </li>
+          </ul>
+          <div></div>
+        </div>
+      </nav>
+    </header>
   );
 }
 
